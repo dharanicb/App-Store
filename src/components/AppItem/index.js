fca5d339bbe9b8d112f2b8ac8c9cb7adb96c5363
dharanicb <dharanicb86391@gmail.com>
2023-04-09 15:19:05 +0000

    solution updated

--- src/components/AppItem/index.js
+++ src/components/AppItem/index.js
@@ -3,15 +3,11 @@ import './index.css'
 
 const AppItem = props => {
   const {appDetails} = props
-  const {appId, appName, imageUrl} = appDetails
+  const {appName, imageUrl} = appDetails
   return (
     <>
       <li className="project-item-container">
-        <img
-          className="project-item-image"
-          src={imageUrl}
-          alt={`${appName}${appId}`}
-        />
+        <img className="project-item-image" src={imageUrl} alt={appName} />
         <p className="project-item-title">{appName}</p>
       </li>
     </>
